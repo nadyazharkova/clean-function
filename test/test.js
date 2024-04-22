a7342e1f883291682f937a90fccc55614534f8fd
Nadezhda Zharkova <nadezhda.a.zharkova@gmail.com>
2024-04-22 18:59:38 +0500

    commit

--- test/test.js
+++ test/test.js
@@ -2,33 +2,33 @@ import { checkLifes } from '../src/lifesCheck';
 
 test ('checkLifes more than 50', () => {
     const character = {
-            name: 'Marta',
-            health: 90
-        }
+        name: 'Marta',
+        health: 90
+    };
 
-    let result = checkLifes(character)
+    let result = checkLifes(character);
 
-    expect(result).toBe("healthy")
-})
-
-test ('checkLifes more than 50', () => {
-    const character = {
-            name: 'Holly',
-            health: 50
-        }
-
-    let result = checkLifes(character)
-
-    expect(result).toBe("wounded")
-})
+    expect(result).toBe("healthy");
+});
 
 test ('checkLifes more than 50', () => {
     const character = {
-            name: 'Nasty',
-            health: 14
-        }
+        name: 'Holly',
+        health: 50
+    };
 
-    let result = checkLifes(character)
+    let result = checkLifes(character);
 
-    expect(result).toBe("critical")
-})
+    expect(result).toBe('wounded');
+});
+
+test ('checkLifes more than 50', () => {
+    const character = {
+        name: 'Nasty',
+        health: 14
+    };
+
+    let result = checkLifes(character);
+
+    expect(result).toBe('critical');
+});
--- webpack.config.js
+++ webpack.config.js
@@ -3,44 +3,38 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/lifesCheck.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-          },
+    output: { path: path.resolve(__dirname, 'dist'), },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: { loader: 'babel-loader', },
+            },
+            {
+                test: /\.html$/,
+                use: [ { loader: 'html-loader', }, ],
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader', 'css-loader'
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            }
         ],
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      }
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html',
+        })
     ],
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: './src/index.html',
-      filename: './index.html',
-    })
-  ],
 };
