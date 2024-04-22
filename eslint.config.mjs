d951173a9e28bdb7231cd49c6d226f3b1c7ac976
Nadezhda Zharkova <nadezhda.a.zharkova@gmail.com>
2024-04-22 19:13:15 +0500

    commit2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,27 @@ import pluginJs from '@eslint/js';
 import stylisticJs from '@stylistic/eslint-plugin-js';
 
 export default [
+    module.exports = {
+        'env': {
+          'browser': true,
+          'es2021': true,
+          'node': true
+        },
+        'extends': 'eslint:recommended',
+        'overrides': [
+          {
+            'files': ['**/*/*.test.js'],
+            'plugins': ['jest'],
+            'extends': ['plugin:jest/recommended'],
+            'rules': { 'jest/prefer-expect-assertions': 'off' }
+          }
+        ],
+        'parserOptions': {
+          'ecmaVersion': 'latest',
+          'sourceType': 'module'
+        },
+        'rules': {}
+    },
     pluginJs.configs.recommended,
     {
         ignores: [
